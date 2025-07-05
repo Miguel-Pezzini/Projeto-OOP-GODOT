27da41e004f9398d1d3670ce48a0b1d3af6a012d
Miguel Pezzini Kuhr <miguel.kuhr@gmail.com>
2025-07-05 11:54:35 -0300

    feat: Create second map

---
+++ scripts/SunnylandSpike.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public partial class SunnylandSpike : Area2D
+{
+	public override void _Ready()
+	{
+		BodyEntered += OnBodyEntered;
+	}
+
+	private void OnBodyEntered(Node2D body)
+	{
+		if (body.IsInGroup("player"))
+		{
+			GD.Print("entrou");
+			GetTree().ReloadCurrentScene();
+		}
+	}
+}
